966af36352d4ffc9311850e143c90c5b9dc36ece
greggameplayer <33609333+greggameplayer@users.noreply.github.com>
2018-01-09 10:02:35 +0100

    add Chauve-Souris webpage

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-﻿
+﻿var aveugle;
+var cachalot;
+var chauvesouris;
+function onStart(){
+	aveugle = document.getElementById("aveugle");
+	cachalot = document.getElementById("Cachalot");
+	chauvesouris = document.getElementById("C-S");
+	aveugle.addEventListener("click", onAveugleClick);
+	cachalot.addEventListener("click", onCachalotClick);
+	chauvesouris.addEventListener("click", onChauvesourisClick);
+}
+window.addEventListener("load", onStart);
+
+
+function onAveugleClick(){
+	document.location.href="aveugle.html";
+}
+function onCachalotClick(){
+	document.location.href="cachalot.html";
+}
+function onChauvesourisClick(){
+	document.location.href="chauvesouris.html";
+}
