f887fce6c6dac53fff337d6fa2732984635f2c3b
greggameplayer <33609333+greggameplayer@users.noreply.github.com>
2018-02-17 16:10:00 +0100

    Rework of the home page
    - change the disposition of the text, image, etc
    - add a return to home button
    - reworked index2 button

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
 ﻿var aveugle;
 var chauvesouris;
-function onStart(){
-	aveugle = document.getElementById("aveugle");
-	chauvesouris = document.getElementById("C-S");
-	aveugle.addEventListener("click", onAveugleClick);
-	chauvesouris.addEventListener("click", onChauvesourisClick);
-}
-window.addEventListener("load", onStart);
+var home;
+
 
 
 function onAveugleClick(){
@@ -15,3 +10,20 @@ function onAveugleClick(){
 function onChauvesourisClick(){
 	document.location.href="chauvesouris.html";
 }
+function onHomeClick(){
+	document.location.href="index.html";
+}
+
+function onStart(){
+	if(document.body.contains(document.getElementById("aveugle")) == true && document.body.contains(document.getElementById("C-S")) == true){
+	aveugle = document.getElementById("aveugle");
+	chauvesouris = document.getElementById("C-S");
+	aveugle.addEventListener("click", onAveugleClick);
+	chauvesouris.addEventListener("click", onChauvesourisClick);
+	}
+	if(document.body.contains(document.getElementById("home")) == true){
+	home = document.getElementById("home");
+	home.addEventListener("click", onHomeClick);
+	}
+}
+window.addEventListener("load", onStart);
