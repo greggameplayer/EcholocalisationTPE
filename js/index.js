2a9e6dd32aa9fe52fb5d1715d89048e4b198921f
greggameplayer <33609333+greggameplayer@users.noreply.github.com>
2018-02-17 16:19:09 +0100

    correct eqeqeq code style error

--- js/index.js
+++ js/index.js
@@ -15,13 +15,13 @@ function onHomeClick(){
 }
 
 function onStart(){
-	if(document.body.contains(document.getElementById("aveugle")) == true && document.body.contains(document.getElementById("C-S")) == true){
+	if(document.body.contains(document.getElementById("aveugle")) === true && document.body.contains(document.getElementById("C-S")) === true){
 	aveugle = document.getElementById("aveugle");
 	chauvesouris = document.getElementById("C-S");
 	aveugle.addEventListener("click", onAveugleClick);
 	chauvesouris.addEventListener("click", onChauvesourisClick);
 	}
-	if(document.body.contains(document.getElementById("home")) == true){
+	if(document.body.contains(document.getElementById("home")) === true){
 	home = document.getElementById("home");
 	home.addEventListener("click", onHomeClick);
 	}
