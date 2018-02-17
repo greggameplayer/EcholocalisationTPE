d5b2a5864442704b6f5d97d1f6389a8a285f94a0
greggameplayer <33609333+greggameplayer@users.noreply.github.com>
2018-02-17 14:41:37 +0100

    Delete cachalot image

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
 ﻿var aveugle;
-var cachalot;
 var chauvesouris;
 function onStart(){
 	aveugle = document.getElementById("aveugle");
-	cachalot = document.getElementById("Cachalot");
 	chauvesouris = document.getElementById("C-S");
 	aveugle.addEventListener("click", onAveugleClick);
-	cachalot.addEventListener("click", onCachalotClick);
 	chauvesouris.addEventListener("click", onChauvesourisClick);
 }
 window.addEventListener("load", onStart);
@@ -14,9 +11,6 @@ window.addEventListener("load", onStart);
 
 function onAveugleClick(){
 	document.location.href="aveugle.html";
-}
-function onCachalotClick(){
-	document.location.href="cachalot.html";
 }
 function onChauvesourisClick(){
 	document.location.href="chauvesouris.html";
